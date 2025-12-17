9fd70408a0f1a3f173993ae166a75e30e122a3e7
Urs Mueller <ursmueller2005@gmail.com>
2025-12-17 16:37:32 +0100

    addes requirements

---
+++ mockup_ui.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import ui_device 
+
+st.write("# Mockup UI für Gerätemanagement")
+st.write("Dies ist ein Mockup UI, das das Gerätemanagement demonstriert.")
+st.write("## Gerätemanagement Modul")
+ui_device
+
+st.write("## Ende des Mockup UI")
